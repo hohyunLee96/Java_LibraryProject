799f9e50eb2a7955f80e0c3574ce019a1f28f2b5
hohyunLee96 <90310107+hohyunLee96@users.noreply.github.com>
2023-05-24 23:35:55 +0200

    Add files via upload

---
+++ JavaFinalAssignment_HohyunLee_683070/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module nl.inholland.javafinalassignment_hohyunlee_683070 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens nl.inholland.javafinalassignment_hohyunlee_683070 to javafx.fxml;
+    exports nl.inholland.javafinalassignment_hohyunlee_683070;
+}
---
+++ JavaFinalAssignment_HohyunLee_683070/src/main/java/nl/inholland/javafinalassignment_hohyunlee_683070/HelloController.java
@@ -0,0 +1,14 @@
+package nl.inholland.javafinalassignment_hohyunlee_683070;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
